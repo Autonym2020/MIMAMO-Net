73685b09506bd7c6549fa3ebf1bf2679abb117e6
Ubuntu <ubuntu@ip-172-31-34-134.ec2.internal>
2020-07-01 22:32:32 +0000

    Updated

--- api/run_example.py
+++ api/run_example.py
@@ -1,8 +1,15 @@
 import os
 from tester import Tester
 import time
-example_video = 'refactor/utterance_1.mp4'
+import sys 
+import os.path 
 model_weight_path = 'models/model_weights.pth.tar'
+argv = sys.argv[1]
+
+if os.path.exists(argv):
+	example_video = sys.argv[1]
+else:
+	print("File does not exist")
 
 tester = Tester(model_weight_path, batch_size=64, workers=8, quiet=True)
 tic = time.time()
